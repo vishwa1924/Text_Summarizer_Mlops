ab962e9d64fc94d89df38a97617d8c47f0872786
vishwa <saivishwa.gaddam.com>
2023-08-21 13:31:15 -0700

    data validation

---
+++ src/Text_Summarizer/pipeline/stage02_data_validation.py
@@ -0,0 +1,14 @@
+from Text_Summarizer.config.configuration import ConfigurationManager
+from Text_Summarizer.entity.config_entity import DataValidationConfig
+from Text_Summarizer.components.data_validation import DataValidation
+
+class DataValidationTrainingPipeline:
+    def __init__(self):
+        pass
+
+    def main(self):
+            config = ConfigurationManager()
+            data_validation_config = config.get_data_validation_config()
+            data_validation = DataValidation(config=data_validation_config)
+            data_validation.validate_all_files_exist()
+        
